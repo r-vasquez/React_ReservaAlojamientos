746cedb42a59d60c858673622354c066e73fcd89
r-vasquez <rvasque3@gmail.com>
2020-08-02 19:52:39 -0500

    working basic React App with CDN

---
+++ Rogger_Vasquez/src/App.js
@@ -0,0 +1,12 @@
+import Prueba from './components/Prueba';
+
+function App() {
+  return (
+    <div>
+      <h1>App Works HOLAA</h1>
+      <Prueba />
+    </div>
+  );
+}
+
+ReactDOM.render(<App />, document.getElementById('app'));
---
+++ Rogger_Vasquez/src/components/Prueba.js
@@ -0,0 +1,6 @@
+class Prueba extends React.Component {
+  render() {
+    return <h2>Prueba Works!</h2>;
+  }
+}
+export default Prueba;
