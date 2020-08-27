9be5784129db0fa7351639c9606910576a2c86b3
r-vasquez <rvasque3@gmail.com>
2020-08-26 22:05:10 -0500

    separating date utils to a external js file

---
+++ Rogger_Vasquez/scripts/dateLib.js
@@ -0,0 +1,86 @@
+/*
+  r-vasquez:
+  Library of functions to parse date object into a pre-defined spanish function
+  w/o using external libs.
+
+  Excercise purpose only... should have used moment.js
+  */
+
+const parseDay = function (day) {
+  switch (day) {
+    case 0:
+      return 'domingo';
+      break;
+    case 1:
+      return 'lunes';
+      break;
+    case 2:
+      return 'martes';
+      break;
+    case 3:
+      return 'miercoles';
+      break;
+    case 4:
+      return 'jueves';
+      break;
+    case 5:
+      return 'viernes';
+      break;
+    case 6:
+      return 'sábado';
+      break;
+    default:
+      break;
+  }
+};
+
+const parseMonth = function (Month) {
+  switch (Month) {
+    case 0:
+      return 'enero';
+      break;
+    case 1:
+      return 'febrero';
+      break;
+    case 2:
+      return 'marzo';
+      break;
+    case 3:
+      return 'abril';
+      break;
+    case 4:
+      return 'mayo';
+      break;
+    case 5:
+      return 'junio';
+      break;
+    case 6:
+      return 'julio';
+      break;
+    case 7:
+      return 'agosto';
+      break;
+    case 8:
+      return 'septiembre';
+      break;
+    case 9:
+      return 'octubre';
+      break;
+    case 10:
+      return 'noviembre';
+      break;
+    case 11:
+      return 'diciembre';
+      break;
+    default:
+      break;
+  }
+};
+
+export const parseDate = function (date) {
+  let weekday = parseDay(date.getDay());
+  let day = date.getDate();
+  let month = parseMonth(date.getMonth());
+  let year = date.getFullYear();
+  return `${weekday} ${day} de ${month}, ${year}`;
+};
--- Rogger_Vasquez/src/components/MainPage.js
+++ Rogger_Vasquez/src/components/MainPage.js
@@ -1,6 +1,7 @@
 import Filters from './Filters';
 import TopBar from './TopBar';
 import CardContainer from './CardContainer';
+import { parseDate } from '../../scripts/dateLib';
 
 class MainPage extends React.Component {
   constructor() {
@@ -16,98 +17,13 @@ class MainPage extends React.Component {
 
   handleIniDate(inidate) {
     this.setState({
-      iniDate: this.parseDate(inidate)
+      iniDate: parseDate(inidate)
     });
   }
   handleFinalDate(finaldate) {
     this.setState({
-      finalDate: this.parseDate(finaldate)
+      finalDate: parseDate(finaldate)
     });
-  }
-
-  //TODO : Move dates Scripts to external JS
-  /*
-  r-vasquez:
-  Receives a Date Object and parse it to a string
-  w/o using external libs.
-  */
-  parseDate(date) {
-    let weekday = this.parseDay(date.getDay());
-    let day = date.getDate();
-    let month = this.parseMonth(date.getMonth());
-    let year = date.getFullYear();
-    return `${weekday} ${day} de ${month}, ${year}`;
-  }
-
-  parseDay(day) {
-    switch (day) {
-      case 0:
-        return 'domingo';
-        break;
-      case 1:
-        return 'lunes';
-        break;
-      case 2:
-        return 'martes';
-        break;
-      case 3:
-        return 'miercoles';
-        break;
-      case 4:
-        return 'jueves';
-        break;
-      case 5:
-        return 'viernes';
-        break;
-      case 6:
-        return 'sábado';
-        break;
-      default:
-        break;
-    }
-  }
-
-  parseMonth(Month) {
-    switch (Month) {
-      case 0:
-        return 'enero';
-        break;
-      case 1:
-        return 'febrero';
-        break;
-      case 2:
-        return 'marzo';
-        break;
-      case 3:
-        return 'abril';
-        break;
-      case 4:
-        return 'mayo';
-        break;
-      case 5:
-        return 'junio';
-        break;
-      case 6:
-        return 'julio';
-        break;
-      case 7:
-        return 'agosto';
-        break;
-      case 8:
-        return 'septiembre';
-        break;
-      case 9:
-        return 'octubre';
-        break;
-      case 10:
-        return 'noviembre';
-        break;
-      case 11:
-        return 'diciembre';
-        break;
-      default:
-        break;
-    }
   }
 
   render() {
