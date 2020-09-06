040e6c859514c9f5e6686127a17088aaf7732863
Rogger <rvasque3@gmail.com>
2020-09-06 12:54:57 -0500

    Removin unnecesary comments

--- Rogger_Vasquez/src/components/Card.js
+++ Rogger_Vasquez/src/components/Card.js
@@ -1,17 +1,4 @@
 class Card extends React.Component {
-  // {
-  //   slug: 'la-bamba-de-areco',
-  //   name: 'La Bamba de Areco',
-  //   photo: './images/la-bamba-de-areco.jpg',
-  //   description:
-  //     'La Bamba de Areco está ubicada en San Antonio de Areco, en el corazón de la pampa. Es una de las estancias más antiguas de la Argentina, recientemente restaurada para ofrecer a sus huéspedes todo el confort y esplendor colonial.',
-  //   availabilityFrom: today.valueOf(),
-  //   availabilityTo: today.valueOf() + 864000000, // 10 days
-  //   rooms: 11,
-  //   city: 'Buenos Aires',
-  //   country: 'Argentina',
-  //   price: 4
-  // }
 
   render() {
     return (
